c36a198e096e95967fced5c8451f84052f39df27
dclement8 <dylanclement7@protonmail.ch>
2018-06-05 15:52:09 +0200

    update docs

--- server/api_project.js
+++ server/api_project.js
@@ -20,7 +20,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-05T00:56:46.501Z",
+    "time": "2018-06-05T13:51:00.570Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
